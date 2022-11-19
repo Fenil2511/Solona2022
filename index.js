ae2ff5cf4c7b489137bd0c1eb174e59aa3475938
Fenil chauhan <fenilchauhan349@gmail.com>
2022-11-19 10:37:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-const a = 10;
-const b = 11;
-
-function square(x) {
-  return x * x;
-}
-
 var cowsay = require("cowsay");
-const Quote = require('inspirational-quotes');
-console.log(cowsay.say({
-    text : Quote.getQuote().text,
-    e : "oO",
-    T : "U "
-}));
-
-
-
+const Quote = require("inspirational-quotes");
+console.log(
+  cowsay.say({
+    text: Quote.getQuote().text,
+    e: "oO",
+    T: "U ",
+  })
+);
